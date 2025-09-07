078f1b54a9481086ead54db1a34a23b399080d1c
Poojit-Matukumalli <poojit.matukumalli@gmail.com>
2025-09-07 18:18:34 +0530

    Updated SPowerShell.py
    added a calc <expression> command (eval free) and an edit command which replicates the terminal #notepad <filename> command

--- SPowerShell.py
+++ SPowerShell.py
@@ -1,10 +1,12 @@
 import os
 from datetime import datetime
+from re import fullmatch
+print("-"*100)
 print("Sparrow PowerShell")
 print("Copyright (C) Sparrow Corporation. All rights reserved\n") # Dont take this seriously 🙏
-print("Welcome to Sparrow Powershell\n\n")
+print(f"Welcome to Sparrow Powershell\n")
+print("\nType 'help' for a list of commands")
 
-# Please help me improve this cmd_help
 def cmd_help():
     print("""
 Sparrow PowerShell - List of Available commands :
@@ -18,9 +20,11 @@ Sparrow PowerShell - List of Available commands :
 7. pwd                       -> outputs the present directory the user is in
 8. gc                        -> Opens the content of files in pwd
 9. cd <path>                 -> Changes the current directory to <path>
-10. exit                     -> exits the Sparrow PowerShell
+10. calc <expr>              -> calculates an expression
+11. edit <file editor>       -> Edits the file
+12. exit                     -> exits the Sparrow PowerShell
 """)
-    
+
 def cmd_echo(x):  # This is echo
     print(x)
 
@@ -32,7 +36,7 @@ def cmd_del(file_path):
         if file_path == r"C:\Windows\System32":
             print("Cannot remove Critical system files")
         elif os.path.exists(file_path):
-            os.remove(file_path)                                               # Well, This is the file deletion segment
+            os.remove(file_path)                            # Well, This is the file deletion segment
         else:
             print(f"File '{file_path}' not found.")
     except PermissionError:
@@ -63,8 +67,19 @@ def cmd_gc(filename): #gc, Get-Contents
             print(f.read())
     except Exception as e:
         print(f"An error occurred:\n{e}")
-    
-    
+
+def cmd_calc(x):
+    def mathexpr(s):
+        return bool(fullmatch(r"[0-9+\-*/().\s]+",s))
+    if mathexpr(x) == True:
+        print(eval(x))
+    else:
+        print("Enter a valid expression")
+
+def cmd_edit(x):
+    os.system(x)
+
+
 # List of commands
 commands = {
     "help" : cmd_help,
@@ -76,18 +91,20 @@ commands = {
     "ls" : cmd_ls,
     "pwd" : cmd_pwd,
     "cd" : cmd_cd,
-    "gc" : cmd_gc
+    "gc" : cmd_gc,
+    "calc" : cmd_calc,
+    "edit" : cmd_edit
 }
-
 
 while True:                                                 #Looks cluttered and is cluttered
     try:                                                    # For the changing directories in the PowerShell prompt
         cwd = os.getcwd()    
-        Input = input(f"S-PS {cwd}> ").split()              # Input
+        Input = input(f"S-PS {cwd}> ").split()
+        
         if Input[0] in commands:                            # The first token of input checking 
             if len(Input) > 1:
-                commands[Input[0]](" ".join(Input[1:]))     #If input > 1, it adds the Input[1] token in brackets beside the Input[0], Basically a function call
-            else:
+                commands[Input[0]](" ".join(Input[1:]))     #If input > 1, it adds the Input[1] token in parentheses beside the Input[0], Basically a function call
+            else:                                           #input[1:] is slicing, Excludes input[0] and joins the rest to the initial function call
                 commands[Input[0]]()                        # If it's not >1, it just becomes the function call.
         elif Input[0] == "exit":
             break
